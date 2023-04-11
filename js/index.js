b76cb02e688a058a3e7b31ce9d481cd35346e0bb
SarahMumbua <mumbuasarah2030@gmail.com>
2023-04-11 17:20:56 +0300

    modified index.js to make api call and return rsults

---
+++ js/index.js
@@ -0,0 +1,26 @@
+const searchButton = document.querySelector('#search-button');
+const searchInput = document.querySelector('#search-input');
+
+searchButton.addEventListener('click', () => {
+  const query = searchInput.value;
+  fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${query}`)
+    .then(response => response.json())
+    .then(data => {
+      const searchResults = document.querySelector('.col-md-8');
+      searchResults.innerHTML = '';
+      data.meals.forEach(meal => {
+        const card = `
+          <div class="card mb-3">
+            <img class="card-img-top" src="${meal.strMealThumb}" alt="${meal.strMeal}">
+            <div class="card-body">
+              <h5 class="card-title">${meal.strMeal}</h5>
+              <p class="card-text">Category: ${meal.strCategory}</p>
+              <a href="${meal.strSource}" class="btn btn-primary">View Recipe</a>
+            </div>
+          </div>
+        `;
+        searchResults.insertAdjacentHTML('beforeend', card);
+      });
+    })
+    .catch(error => console.log(error));
+});
